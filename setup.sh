493d809537f53dafce7d6ecbca68dde1139e99a6
Evangelos Lioudakis <elioudakis123@gmail.com>
2022-04-10 15:27:35 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+git clone https://github.com/material-shell/material-shell
+cd material-shell
+make install
+git clone https://github.com/Evangelospro/ZSHRC/
+cd ZSHRC
+./setup.sh
+./install-packages.sh
