8bd07136e3f8ce5c7a729fe2ed7fa62db797c22d
Evangelos Lioudakis <elioudakis123@gmail.com>
2022-04-10 16:06:05 +0300

    Update install-packages.py

--- install-packages.py
+++ install-packages.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 packages = open('packages.txt', 'r')
-for package in packages.readlines():
-    package_name = package.split('/')[0]
+for package_name in packages.readlines():
     response = input(f"Do you want to install {package_name} (y/n)")
     if response == 'y':
         print(f"Installing {package_name}...")
